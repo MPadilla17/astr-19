24fe117153ba94c56b25a8c31fb091b3c7e667ce
MPadilla17 <146883888+MPadilla17@users.noreply.github.com>
2023-10-16 14:31:19 -0700

    Add files via upload
    Coding for journal prompt 1.

---
+++ Code-Prompt-1.py
@@ -0,0 +1,14 @@
+#This program will write
+#I am Marco Padilla, He/Him, and I love to eat gorditas and watch Brooklyn Nine-Nine
+def PrintPrompt1():
+	print("Hello, I am Marco Padilla. I use He/Him pronouns. My favorite food is gorditas and my favorite show is Brooklyn Nine-Nine.")
+
+	#This defines my main()
+	#function of the program
+def main ():
+	PrintPrompt1()
+
+#When I run this program, this executes first
+if __name__=="__main__":
+	main()
+
